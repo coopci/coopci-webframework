f250e6c561df78baf276a1f18270d605a5112b43
cooper <cooper@xinwaihui.com>
2018-02-10 15:17:17 +0800

    add VersionHandler

---
+++ src/gubo/http/grizzly/VersionHandler.java
@@ -0,0 +1,27 @@
+package gubo.http.grizzly;
+
+import java.util.HashMap;
+
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+
+import api.handlers.BaseApiHander;
+import api.handlers.IJDBCGetter;
+import gubo.scm.Version;
+
+// 报告当前程序的代码版本
+public class VersionHandler extends BaseApiHander implements IJDBCGetter {
+
+	@Override
+	public Object doGet(Request request, Response response) throws Exception {
+		HashMap<String, Object> ret = this.getOKResponse();
+		
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("svnVersion", Version.svnVersion);
+		data.put("gitVersion", Version.gitVersion);
+		
+		ret.put("data", data);
+		return ret;
+	}
+	
+}
