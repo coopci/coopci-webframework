196c9951303dc4d1c6200664fcef818db84f264e
coopci <bo.coopci@gmail.com>
2018-02-14 00:00:22 +0800

    add /datatable/serverprocess/SqlOrderBy

---
+++ test/unit/gubo/datatable/serverprocess/SqlOrderByTest.java
@@ -0,0 +1,28 @@
+package gubo.datatable.serverprocess;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class SqlOrderByTest {
+
+	@Test
+	public void t1() {
+		
+		String[] columnnames = new String[]{"col0","col1","col2","col3"};
+		Map<String, String> dtParams = new HashMap();
+		
+			
+		dtParams.put("order[0][column]", "3");
+		dtParams.put("order[0][dir]", "desc");
+		
+		dtParams.put("order[1][column]", "2");
+		
+		
+		
+		String orderby = SqlOrderBy.toSqlOrderBy(dtParams, columnnames, "mysql");
+		
+		System.out.println(orderby);
+	}
+}
