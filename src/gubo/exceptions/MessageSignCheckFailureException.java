7a1c717bf644e635dc61cd2f91bfcac1b4bf3880
cooper <cooper@xinwaihui.com>
2018-03-03 16:01:40 +0800

    add MessageSignCheckFailureException

--- src/gubo/exceptions/MessageSignCheckFailureException.java
+++ src/gubo/exceptions/MessageSignCheckFailureException.java
@@ -1,6 +1,6 @@
 package gubo.exceptions;
 
-public class BadParameterException extends ApiException {
+public class MessageSignCheckFailureException extends ApiException {
 
 	/**
 	 * 
@@ -8,7 +8,7 @@ public class BadParameterException extends ApiException {
 	private static final long serialVersionUID = 428638368019065199L;
 	protected String message;
 
-	public BadParameterException(String message) {
+	public MessageSignCheckFailureException(String message) {
 		super();
 		this.message = message;
 	}
@@ -22,6 +22,6 @@ public class BadParameterException extends ApiException {
 	
 	@Override
 	public int getHttpStatus() {
-		return 200;
+		return 400;
 	}
 }
