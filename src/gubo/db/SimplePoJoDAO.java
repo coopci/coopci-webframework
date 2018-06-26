3869b495be51fc2163221fd5fae1e1a60896f935
coopci <bo.coopci@gmail.com>
2018-06-26 23:26:45 +0800

    add SimplePoJoDAO.update

--- src/gubo/db/SimplePoJoDAO.java
+++ src/gubo/db/SimplePoJoDAO.java
@@ -2,38 +2,66 @@ package gubo.db;
 
 import gubo.jdbc.mapping.InsertStatementGenerator;
 import gubo.jdbc.mapping.ResultSetMapper;
+import gubo.jdbc.mapping.UpdateStatementGenerator;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Set;
 
 import javax.sql.DataSource;
 
 public class SimplePoJoDAO {
-	
+
 	final private Class<?> clazz;
+
 	public SimplePoJoDAO(Class<?> clazz) {
 		this.clazz = clazz;
 	}
-	
-	public <T> T loadPoJo(DataSource ds,
-			String sql, Object... params) throws SQLException {
-		
+
+	public <T> T loadPoJo(DataSource ds, String sql, Object... params)
+			throws SQLException {
+
 		Connection dbconn = ds.getConnection();
 		try {
 			ResultSetMapper<T> mapper = new ResultSetMapper<T>();
 			T pojo = mapper.loadPojo(dbconn, this.clazz, sql, params);
-			return pojo;	
+			return pojo;
 		} finally {
 			dbconn.close();
 		}
 	}
-	
-	public <T extends ISimplePoJo> T insert(DataSource ds, T pojo) throws Exception {
+
+	public <T extends ISimplePoJo> T insert(DataSource ds, T pojo)
+			throws Exception {
 		Connection dbconn = ds.getConnection();
 		try {
 			dbconn.setAutoCommit(false);
 			Long newid = InsertStatementGenerator.insertNew(dbconn, pojo);
 			pojo.setId(newid);
+			return pojo;
+		} finally {
+			dbconn.close();
+		}
+	}
+
+	public <T extends ISimplePoJo> T update(DataSource ds, T pojo)
+			throws Exception {
+		Connection dbconn = ds.getConnection();
+		try {
+			dbconn.setAutoCommit(false);
+			UpdateStatementGenerator.update(dbconn, pojo);
+			return pojo;
+		} finally {
+			dbconn.close();
+		}
+	}
+
+	public <T extends ISimplePoJo> T update(DataSource ds, T pojo,
+			Set<String> allowedCols) throws Exception {
+		Connection dbconn = ds.getConnection();
+		try {
+			dbconn.setAutoCommit(false);
+			UpdateStatementGenerator.update(dbconn, pojo, allowedCols);
 			return pojo;
 		} finally {
 			dbconn.close();
