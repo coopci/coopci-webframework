94973387742f33987944b6900a5a52b850ac045d
coopci <bo.coopci@gmail.com>
2018-06-26 22:48:17 +0800

    add  insert(DataSource ds, T pojo)

--- src/gubo/db/SimplePoJoDAO.java
+++ src/gubo/db/SimplePoJoDAO.java
@@ -1,5 +1,6 @@
 package gubo.db;
 
+import gubo.jdbc.mapping.InsertStatementGenerator;
 import gubo.jdbc.mapping.ResultSetMapper;
 
 import java.sql.Connection;
@@ -22,6 +23,18 @@ public class SimplePoJoDAO {
 			ResultSetMapper<T> mapper = new ResultSetMapper<T>();
 			T pojo = mapper.loadPojo(dbconn, this.clazz, sql, params);
 			return pojo;	
+		} finally {
+			dbconn.close();
+		}
+	}
+	
+	public <T extends ISimplePoJo> T insert(DataSource ds, T pojo) throws Exception {
+		Connection dbconn = ds.getConnection();
+		try {
+			dbconn.setAutoCommit(false);
+			Long newid = InsertStatementGenerator.insertNew(dbconn, pojo);
+			pojo.setId(newid);
+			return pojo;
 		} finally {
 			dbconn.close();
 		}
