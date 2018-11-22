6d8197e2390e26fe68e55decd0a49d109d249ea1
Gu Bo <bgu@zuora.com>
2018-11-22 17:17:06 +0800

    add new attibute method to MappingToPath

--- src/gubo/http/grizzly/handlergenerator/MappingToPath.java
+++ src/gubo/http/grizzly/handlergenerator/MappingToPath.java
@@ -2,8 +2,12 @@ package gubo.http.grizzly.handlergenerator;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
+@Target(value={ElementType.METHOD})
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface MappingToPath {
 	String value();
+	String method() default "POST";
 }
