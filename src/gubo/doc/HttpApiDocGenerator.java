639b39cb78cf453de5851e561cb171b7bdee33f6
Gu Bo <bgu@zuora.com>
2018-11-29 17:53:37 +0800

    HttpApiDocGenerator 生成的curl命令加上GET和POST参数

--- src/gubo/doc/HttpApiDocGenerator.java
+++ src/gubo/doc/HttpApiDocGenerator.java
@@ -1,10 +1,10 @@
 package gubo.doc;
 
 import gubo.http.grizzly.handlergenerator.MappingToPath;
-import gubo.http.grizzly.handlergenerator.HandlerGenerator.SourceFile;
+import gubo.http.querystring.QueryStringBinder;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
-import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,9 +17,13 @@ public class HttpApiDocGenerator {
     
     /**
      *   为 interfc 上所有 同时被 {@link Comment} 和 {@link MappingToPath} 标注的 method 生成文档。
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     * @throws UnsupportedEncodingException 
+     * @throws IllegalArgumentException 
      * 
      **/
-    public String generateDoc(Class<?> interfc, String urlPrefix) {
+    public String generateDoc(Class<?> interfc, String urlPrefix) throws InstantiationException, IllegalAccessException, IllegalArgumentException, UnsupportedEncodingException {
         Method[] methods = interfc.getMethods();
 
         List<String> doc = new LinkedList<String>();
@@ -31,7 +35,7 @@ public class HttpApiDocGenerator {
         return "";
     }
     
-    public String generateDoc(Method method, String urlPrefix) {
+    public String generateDoc(Method method, String urlPrefix) throws InstantiationException, IllegalAccessException, IllegalArgumentException, UnsupportedEncodingException {
         Comment comment = method.getAnnotation(Comment.class);
         MappingToPath mappingToPath = method.getAnnotation(MappingToPath.class);
         if (comment == null) {
@@ -40,11 +44,36 @@ public class HttpApiDocGenerator {
         if (mappingToPath == null) {
             return "";
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(urlPrefix + mappingToPath.value());
-        sb.append("\n");
-        sb.append(comment.value());
         
+        String url = urlPrefix + mappingToPath.value();
+        String httpMethod = mappingToPath.method().toUpperCase();
+        StringBuilder sb = new StringBuilder();
+        sb.append(url);sb.append("\n");
+        sb.append("method: " + httpMethod);sb.append("\n");
+        sb.append(comment.value());sb.append("\n");
+
+        String postData = "";
+        String querystring = "";
+        
+        
+        Class<?> parameterType = method.getParameters()[0].getType();
+        
+        Object parameterObj = parameterType.newInstance();
+        
+        QueryStringBinder binder = new QueryStringBinder();
+        String ret = binder.toQueryString(parameterObj, null);
+            
+            
+        if ("POST".equals(httpMethod)) {
+            postData = " -d'" + ret + "' ";
+            
+        } else if ("GET".equals(httpMethod)) {
+            querystring = "?"+ret;
+        }
+        
+        String curl = "curl -X" + httpMethod + postData + " '" + url + querystring + "'";
+        
+        sb.append("curl示例: " + curl);sb.append("\n");
         return sb.toString();
     }
     
