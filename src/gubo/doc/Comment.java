952b039548688515060d709dc0220c894e452296
cooper <cooper@xinwaihui.com>
2018-12-22 12:05:00 +0800

    增加since和的precated

--- src/gubo/doc/Comment.java
+++ src/gubo/doc/Comment.java
@@ -12,4 +12,8 @@ import java.lang.annotation.RetentionPolicy;
 public @interface Comment {
     String value() default ""; // 文档的正文。
     String example() default ""; // 如果被描述的主体是变量，那么这里给出一个示例。
+    String since() default "";
+    
+    String deprecatedBy() default "";
+    String deprecatedSince() default "";
 }
