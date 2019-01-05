20acaf235ab5a59f686e2bf447a27a484faae45d
cooper <cooper@xinwaihui.com>
2019-01-05 13:42:17 +0800

    加注释

--- src/gubo/http/querystring/QueryStringField.java
+++ src/gubo/http/querystring/QueryStringField.java
@@ -11,6 +11,8 @@ public @interface QueryStringField {
    Class<? extends IQueryStringFieldParser> deserializer() default NullParser.class;
    boolean ignoreMalFormat() default true;
    boolean required() default true;
+   
+   // hidden的作用是输出为querystring是隐藏。但是toHashMap时不隐藏，因为hidden是指对外隐藏，而toHashMap是为了对内用的。
    boolean hidden() default false;
    // 如果是字符串类型,指定是否允许是""。
    boolean canBeBlank() default true;
