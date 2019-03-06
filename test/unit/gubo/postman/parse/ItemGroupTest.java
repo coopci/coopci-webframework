08a362d0667fa63ce7fa911312f0ff24e4bc0a7d
ming <807527595@qq.com>
2019-03-06 11:38:51 +0800

    完善ItemGroupTest.java

--- test/unit/gubo/postman/parse/ItemGroupTest.java
+++ test/unit/gubo/postman/parse/ItemGroupTest.java
@@ -16,16 +16,22 @@ public class ItemGroupTest {
 		// 路径开头和结尾不能有“/”
 		assertNotEquals("A/B/", parentPath);
 		assertNotEquals("/A/B", parentPath);
+
+		String parentPath2 = ItemGroup.getParentPath("A//B//C");
+		// 路径中间只能有一个“/”
+		assertNotEquals("A/B", parentPath2);
 	}
 
 	@Test
 	public void testIsTopLevel() {
 		assertTrue(ItemGroup.isTopLevel("A"));
 		assertTrue(ItemGroup.isTopLevel("A/"));
+		assertTrue(ItemGroup.isTopLevel("A//"));
 		// 路径开头不能有“/”
 		assertFalse(ItemGroup.isTopLevel("/A"));
 		assertFalse(ItemGroup.isTopLevel("A/B/C"));
 		assertFalse(ItemGroup.isTopLevel("A/B"));
+
 	}
 
 }
