893b4166ae11c0b214acc9f85ea07a067fbcb6e1
coopci <bo.coopci@gmail.com>
2019-04-09 21:50:45 +0800

    Add Base62

---
+++ src/gubo/encode/Base62.java
@@ -0,0 +1,7 @@
+package gubo.encode;
+
+public class Base62 extends BaseN{
+	public Base62() {
+		super("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	}
+}
---
+++ test/unit/gubo/encode/Base62Test.java
@@ -0,0 +1,27 @@
+package gubo.encode;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Base62Test {
+
+	@Test
+	public void testEncodeDecode() {
+		
+		Base62 base62 = new Base62();
+		
+		assertEquals("0", base62.encodeBase10(0));
+		assertEquals("9", base62.encodeBase10(9));
+		assertEquals("a", base62.encodeBase10(10));
+		assertEquals("f", base62.encodeBase10(15));
+		assertEquals("g", base62.encodeBase10(16));
+		assertEquals("z", base62.encodeBase10(35));
+		assertEquals("A", base62.encodeBase10(36));
+		assertEquals("Z", base62.encodeBase10(61));
+		
+		long num = 4723945325L;
+		String encoded = base62.encodeBase10(num);
+		assertEquals(num, base62.decodeBase62(encoded));		
+	}
+}
