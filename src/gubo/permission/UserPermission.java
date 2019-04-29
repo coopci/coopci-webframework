e531f1c44b92c5abf4729ffb385d4ac06475eada
ming <807527595@qq.com>
2019-04-29 14:37:35 +0800

    bugfix

--- src/gubo/permission/UserPermission.java
+++ src/gubo/permission/UserPermission.java
@@ -3,18 +3,25 @@ package gubo.permission;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
+import gubo.http.querystring.QueryStringField;
+
 @Entity(name="v_user_permission")
 public class UserPermission {
 
 	@Column()
+	@QueryStringField()
 	public long user_id;
 	@Column()
+	@QueryStringField()
 	public long role_id;
 	@Column()
+	@QueryStringField()
 	public long permission_id;
 	@Column()
+	@QueryStringField()
 	public String permission;
 	@Column()
+	@QueryStringField()
 	public String permission_code;
 	
 }
