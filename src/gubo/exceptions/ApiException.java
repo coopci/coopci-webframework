d5e17b7d58f1926fbfa46f1801326382ff6f7509
coopci <bo.coopci@gmail.com>
2019-05-04 17:30:08 +0800

    加public ApiException(String msg)

--- src/gubo/exceptions/ApiException.java
+++ src/gubo/exceptions/ApiException.java
@@ -7,6 +7,12 @@ public class ApiException extends Exception {
 	 */
 	private static final long serialVersionUID = 7414527814934184368L;
 
+	public ApiException(String msg) {
+		super(msg);
+	}
+	public ApiException() {
+		
+	}
 	public int getHttpStatus() {
 		return 200;
 	}
