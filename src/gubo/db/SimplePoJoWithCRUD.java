c8e957d403c3b9b7107cd747839c463ed853512a
coopci <bo.coopci@gmail.com>
2019-05-19 22:45:08 +0800

    增加SimplePoJoWithCRUD.java，以后pojo类实现这个方法就可以直接crud了，不需要在写dao.XXX

---
+++ src/gubo/db/SimplePoJoWithCRUD.java
@@ -0,0 +1,54 @@
+package gubo.db;
+
+import gubo.reflection.FieldCopy;
+
+import java.sql.Connection;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class SimplePoJoWithCRUD implements ISimplePoJo {
+
+	static DaoManager daoManager = new DaoManager();
+	@Id()
+	@Column(name = "id")
+	@GeneratedValue
+	public Long id;
+	
+	@Override
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public void insert(Connection dbconn) throws Exception {
+		SimplePoJoDAO dao = daoManager.getDao(this.getClass());
+		dao.insert(dbconn, this);
+	}
+	
+	public void update(Connection dbconn) throws Exception {
+		SimplePoJoDAO dao = daoManager.getDao(this.getClass());
+		dao.update(dbconn, this);
+	}
+	
+	/**
+	 * 这个方法里会把 this 填上。 
+	 * 
+	 * @param dbconn
+	 * @param pkName
+	 * @param pkValue
+	 * @return 是否从数据库里找到了符合条件的 数据。
+	 * @throws Exception
+	 */
+	public boolean loadPoJoByPK(Connection dbconn, String pkName, Object pkValue) throws Exception {
+		SimplePoJoDAO dao = daoManager.getDao(this.getClass());
+		Object o = dao.loadPoJoByPK(dbconn, pkName, pkValue);
+		if (o == null) {
+			return false;
+		}
+		FieldCopy.copy(o, this);
+		return true;
+	}
+
+}
