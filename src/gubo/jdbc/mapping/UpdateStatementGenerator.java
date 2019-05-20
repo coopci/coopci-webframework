a1a797f0acd0dc9ae5c7c0c330950fd5c9936e02
Gu Bo <bgu@zuora.com>
2019-05-20 13:46:18 +0800

    bugfix: 用 FieldUtils.getAllFields(clazz) 替代 clazz.getDeclaredFields()

--- src/gubo/jdbc/mapping/UpdateStatementGenerator.java
+++ src/gubo/jdbc/mapping/UpdateStatementGenerator.java
@@ -14,6 +14,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,7 +53,7 @@ public class UpdateStatementGenerator {
 		}
 
 		StatementContext ret = new StatementContext();
-		Field[] fields = clazz.getDeclaredFields();
+		Field[] fields = FieldUtils.getAllFields(clazz);
 
 		LinkedList<Field> values = new LinkedList<Field>();
 		LinkedList<Field> ids = new LinkedList<Field>();
