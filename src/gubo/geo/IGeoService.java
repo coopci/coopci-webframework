38f71f0e3f2794dd4600f5955ac179688f9a58b0
cooper <cooper@xinwaihui.com>
2019-06-08 14:57:30 +0800

    给IGeoService.getLocationByIpAddr 加默认实现。

--- src/gubo/geo/IGeoService.java
+++ src/gubo/geo/IGeoService.java
@@ -8,7 +8,9 @@ public interface IGeoService {
 	 * @param level GeoLevel里面定义的级别。 
 	 **/
 	default String getLocationByIpAddr(String ipaddr, String level) {
-		
-		throw new UnsupportedOperationException();
+		if (ipaddr.startsWith("127.")) {
+			return "local"; // 本地开发用，就算没连网也能返回。
+		}
+		throw new UnsupportedOperationException(this.getClass() + " not implemented for " + ipaddr);
 	}
 }
