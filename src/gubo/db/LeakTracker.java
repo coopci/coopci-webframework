0422cbc98befa180c7989b3c6d2b4f79b2892ff1
Gu Bo <bgu@zuora.com>
2019-07-05 10:49:24 +0800

    key的类型改成AutoCloseable

--- src/gubo/db/LeakTracker.java
+++ src/gubo/db/LeakTracker.java
@@ -3,26 +3,25 @@ package gubo.db;
 import gubo.doc.Comment;
 import gubo.http.grizzly.handlergenerator.MappingToPath;
 
-import java.sql.Connection;
 import java.util.concurrent.ConcurrentHashMap;
 
 // 追踪jdbc链接泄漏
 public class LeakTracker {
 
-    ConcurrentHashMap<Connection, StackTraceElement[]> track = new ConcurrentHashMap<Connection, StackTraceElement[]> ();
+    ConcurrentHashMap<AutoCloseable, StackTraceElement[]> track = new ConcurrentHashMap<AutoCloseable, StackTraceElement[]> ();
     
-    public void add(Connection conn, StackTraceElement[] stack) {
+    public void add(AutoCloseable conn, StackTraceElement[] stack) {
         track.put(conn, stack);
     }
     
-    public void remove(Connection conn) {
+    public void remove(AutoCloseable conn) {
         this.track.remove(conn);
     }
     
     public static class EmptyRequest {}
     @MappingToPath(value = "/dbconn/leak-track", method = "GET")
     @Comment(value = "链接泄漏。", group = "系统监控")
-    public ConcurrentHashMap<Connection, StackTraceElement[]> login(EmptyRequest req) {
+    public ConcurrentHashMap<AutoCloseable, StackTraceElement[]> getAll(EmptyRequest req) {
         return this.track;
     }
 }
