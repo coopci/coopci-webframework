197222098f76f1c59fbfe060d90410a790d8e37d
coopci <bo.coopci@gmail.com>
2019-08-02 23:03:34 +0800

    make fields public

--- src/gubo/scm/VersionedFile.java
+++ src/gubo/scm/VersionedFile.java
@@ -1,8 +1,8 @@
 package gubo.scm;
 
 public class VersionedFile implements Comparable<VersionedFile>{
-	ThreeDigitsVersion version;
-	String filename;
+	public ThreeDigitsVersion version;
+	public String filename;
 	@Override
 	public int compareTo(VersionedFile other) {
 		return this.version.compareTo(other.version);
