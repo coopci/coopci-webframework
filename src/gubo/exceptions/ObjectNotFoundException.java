3715743373498c871c8e32b242f12c5b162293ae
Gu Bo <bgu@zuora.com>
2019-09-03 15:20:30 +0800

    改ObjectNotFoundException的message

--- src/gubo/exceptions/ObjectNotFoundException.java
+++ src/gubo/exceptions/ObjectNotFoundException.java
@@ -1,14 +1,20 @@
 package gubo.exceptions;
 
+import com.google.common.base.Strings;
+
 public class ObjectNotFoundException extends ApiException {
 
 	private static final long serialVersionUID = 7087901917430232985L;
 	protected String message;
 
 	public ObjectNotFoundException(String message) {
-		super();
-		this.message = "Object not found: " + message;
-	}
+        super();
+        if (Strings.isNullOrEmpty(message)) {
+            this.message = message;
+        } else {
+            this.message = "Object not found";
+        }
+    }
 	public String getMessage() {
 		return this.message;
 	}
