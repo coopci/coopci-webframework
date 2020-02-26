a6117a2610c5694cb13643b8bd85cc98115afc06
coopci <bo.coopci@gmail.com>
2020-02-26 22:01:26 +0800

    print class.getName() and class.getCanonicalName

--- learn/learn/statsd/SimpleMetrics.java
+++ learn/learn/statsd/SimpleMetrics.java
@@ -7,7 +7,8 @@ public class SimpleMetrics {
 	private static final StatsDClient statsd = new NonBlockingStatsDClient("my.prefix", "localhost", 8125);
 
 	public static void main(String args[]) throws InterruptedException {
-		
+			System.out.println(statsd.getClass().getName());
+			System.out.println(statsd.getClass().getCanonicalName());
 		    statsd.incrementCounter("bar");
 		    statsd.recordGaugeValue("baz", 100);
 		    statsd.recordExecutionTime("bag", 25);
